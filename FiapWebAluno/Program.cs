65959c093a04be671ccacbdb3dd5d18b0cc24621
Administrador Future <Administrador Future@ALUGA-062049>
2025-11-26 10:36:18 -0300

    Adicionar arquivos de projeto.

---
+++ FiapWebAluno/Program.cs
@@ -0,0 +1,62 @@
+﻿#region IMPORTAÇÃO REFERENTE AO BANCO DE DADOS
+using Fiap.Web.Alunos.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+#endregion
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+
+builder.Services.AddDbContext<DatabaseContext>(opt =>
+{
+    opt.UseOracle(connectionString);
+
+    // opcional: logs sensíveis só no dev
+    if (builder.Environment.IsDevelopment())
+    {
+        opt.EnableSensitiveDataLogging(true);
+    }
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+
+    try
+    {
+        var ok = db.Database.CanConnect();
+        Console.WriteLine($"✅ Conectou no Oracle? {ok}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("❌ Falhou ao conectar no Oracle:");
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+app.Run();
